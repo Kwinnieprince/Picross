c61398557953d983d8ad8124e5b73a824cfeb957
Kwinnieprince <kwinten@delrue.com>
2019-04-12 11:13:46 +0200

    updates

--- PiCross/View/MainWindow.xaml.cs
+++ PiCross/View/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataStructures;
+using PiCross;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,9 @@ using System.Windows.Shapes;
 
 namespace View
 {
+    using System.Globalization;
+    using Grid = DataStructures.Grid;
+    using size = DataStructures.Size;
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -23,6 +28,52 @@ namespace View
         public MainWindow()
         {
             InitializeComponent();
+            var puzzle = Puzzle.FromRowStrings(
+                "xxxxx",
+                "x...x",
+                "x...x",
+                "x...x",
+                "xxxxx"
+                );
+            var facade = new PiCrossFacade();
+            var playablePuzzle = facade.CreatePlayablePuzzle(puzzle);
+
+            playablePuzzle.Grid[new Vector2D(0, 0)].Contents.Value = Square.FILLED;
+            playablePuzzle.Grid[new Vector2D(0, 0)].Contents.Value = Square.FILLED;
+
+            PicrossControl.Grid = playablePuzzle.Grid;
+            PicrossControl.RowConstraints = playablePuzzle.RowConstraints;
+
         }
     }
+
+    public class SquareConverter : IValueConverter
+    {
+        public object Filled { get; set; }
+        public object Empty { get; set; }
+        public object Unknown { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var square = (Square)value;
+            if (square == Square.EMPTY)
+            {
+                return Empty;
+            }
+            else if (square == Square.FILLED)
+            {
+                return Filled;
+            }
+            else
+            {
+                return Unknown;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 }
---
+++ PiCross/ViewModel/ViewModel.cs
@@ -0,0 +1,79 @@
+﻿using PiCross;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Grid = DataStructures.Grid;
+
+
+namespace ViewModel
+{
+    class ViewModelMainWindow
+    {
+        public ViewModelMainWindow()
+        {
+            this.Puzzle = Puzzle.FromRowStrings(
+                "xxxxx",
+                "x...x",
+                "x...x",
+                "x...x",
+                "xxxxx"
+            );
+
+            this.Facade = new PiCrossFacade();
+
+            this.PlayablePuzzle = Facade.CreatePlayablePuzzle(this.Puzzle);
+
+            this.ClickCommand = new ClickRectangle(this);
+        }
+
+        public Puzzle Puzzle;
+
+        public PiCrossFacade Facade { get; }
+
+        public IPlayablePuzzle PlayablePuzzle { get; }
+
+        public ICommand ClickCommand { get; private set; }
+
+        public class ClickRectangle : ICommand
+        {
+            private ViewModelMainWindow viewModelMainWindow;
+
+            public ClickRectangle(ViewModelMainWindow viewModelMainWindow)
+            {
+                vm = viewModelMainWindow;
+                canExcecute = true;
+            }
+
+            private ViewModelMainWindow vm;
+            private bool canExcecute;
+
+            public EventHandler CanExecuteChange;
+
+            public bool CanExcecute()
+            {
+                return canExcecute;
+            }
+
+            public void Excecute(object parameter)
+            {
+                var square = parameter as IPlayablePuzzleSquare;
+                ChangeValue(square);
+            }
+
+            private void ChangeValue(IPlayablePuzzleSquare square)
+            {
+                if (vm.PlayablePuzzle.Grid[square.Position].Contents.Value == Square.EMPTY || vm.PlayablePuzzle.Grid[square.Position].Contents.Value == Square.UNKNOWN)
+                {
+                    vm.PlayablePuzzle.Grid[square.Position].Contents.Value = Square.FILLED;
+                }
+                else
+                {
+                    vm.PlayablePuzzle.Grid[square.Position].Contents.Value = Square.EMPTY;
+                }
+            }
+        }
+    }
+}
